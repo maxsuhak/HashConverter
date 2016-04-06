e98e40a5f00e2988bc165279a01218504a27a84d
maxsuhak <maxsuhak@gmail.com>
2016-04-06 17:59:09 +0300

    add #to_float

--- hash_converter.rb
+++ hash_converter.rb
@@ -21,6 +21,22 @@ module HashConverter
       convert hash, :to_sym
     end
 
+    # convert hash values to float if BigDecimal
+    def to_float obj
+      case obj
+      when Hash
+        obj.inject({}) do |hash, (key, value)|
+          value = value.to_f if value.kind_of? BigDecimal
+          hash[key] = value
+          hash
+        end
+      when Array
+        obj.map { |mass| to_float mass }
+      else
+        obj
+      end
+    end
+
     # converting
     def convert obj, *method
       case obj
--- hash_converter_spec.rb
+++ hash_converter_spec.rb
@@ -94,4 +94,55 @@ describe HashConverter do
       expect(HashConverter.keys_to_symbol(hash)).to eq(result)
     end
   end
+
+  context '#to_float' do
+    let(:hash) do
+      [{
+        "Zip"                  => "94303",
+        "City"                 => "PALO ALTO",
+        "StateID"              => "CA",
+        "County"               => "SANTA CLARA",
+        "CityAliasMixedCase"   => "East Palo Alto",
+        "FederalUSFPercent"    => BigDecimal("7.1762"),
+        "StateUSFPercent"      => BigDecimal("7.1762"),
+        "State911"             => BigDecimal("7.1762"),
+        "StateUtilityPercent"  => BigDecimal("7.1762"),
+        "StateOther"           => BigDecimal("7.1762"),
+        "County911"            => BigDecimal("7.1762"),
+        "CountyUtilityPercent" => BigDecimal("7.1762"),
+        "CountyUtility"        => BigDecimal("7.1762"),
+        "CountyOther"          => BigDecimal("7.1762"),
+        "City911"              => BigDecimal("7.1762"),
+        "CityUtilityPercent"   => BigDecimal("7.1762"),
+        "CityOther"            => BigDecimal("7.1762"),
+        "RegComplianceFee"     => BigDecimal("7.1762")
+      }]
+    end
+    let(:result) do
+      [{
+        "Zip"                  => "94303",
+        "City"                 => "PALO ALTO",
+        "StateID"              => "CA",
+        "County"               => "SANTA CLARA",
+        "CityAliasMixedCase"   => "East Palo Alto",
+        "FederalUSFPercent"    => 7.1762,
+        "StateUSFPercent"      => 7.1762,
+        "State911"             => 7.1762,
+        "StateUtilityPercent"  => 7.1762,
+        "StateOther"           => 7.1762,
+        "County911"            => 7.1762,
+        "CountyUtilityPercent" => 7.1762,
+        "CountyUtility"        => 7.1762,
+        "CountyOther"          => 7.1762,
+        "City911"              => 7.1762,
+        "CityUtilityPercent"   => 7.1762,
+        "CityOther"            => 7.1762,
+        "RegComplianceFee"     => 7.1762
+      }]
+    end
+
+    it 'should convert hash values to float if BigDecimal' do
+      expect(HashConverter.to_float(hash)).to eq result
+    end
+  end
 end
